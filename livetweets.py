28a05c094eaf5742ba28070240533beb3c6b7f6f
Aaron Griffith <aargri@gmail.com>
2014-03-02 18:47:22 -0500

    s/python/tweetTrek/

--- livetweets.py
+++ livetweets.py
@@ -80,7 +80,7 @@ class Relay(tweepy.StreamListener):
     def on_limit(self, track):
         logging.warning('tweepy limit: ' + str(track))
 
-relay = Relay(track=['python'])
+relay = Relay(track=['tweetTrek'])
 relay.start()
 
 @app.route('/')
