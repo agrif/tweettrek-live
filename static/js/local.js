5b4563d047beac0a9eb56b9e747d501b9b89dd04
Aaron Griffith <aargri@gmail.com>
2014-03-16 21:36:44 -0400

    stopped using default twitter chrome

--- static/js/local.js
+++ static/js/local.js
@@ -1,4 +1,5 @@
 var live = new ReconnectingWebSocket("ws://" + location.host + "/live");
+var template = Handlebars.compile($('#tweet-template').html());
 
 live.onmessage = function(message) {
 	var data = JSON.parse(message.data);
@@ -6,12 +7,17 @@ live.onmessage = function(message) {
 	
 	$('#teaser').hide();
 	
-	container = $("<div></div>");
-	twttr.widgets.createTweet(data.id, container.get()[0], null, {
-		conversation: 'none',
-		cards: 'none'
-	});
-	container.prependTo($('#live'));
+	// format the date all nice
+	var date = new Date(data.created_at);
+	data.nice_date = date.format("mmmm d, yyyy 'at' h:MM TT Z");
+	
+	// link urls in the text for us
+	data.nice_text = Autolinker.link(data.text);
+	
+	// add it
+	var content = $(template(data));
+	content.prependTo($('#live'));
+	content.hide().fadeIn();
 	
 	while ($('#live').children().length > 200) {
 		$('#live').children().last().remove();
